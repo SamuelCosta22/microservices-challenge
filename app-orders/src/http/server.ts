ca4a173f83d7c2dc47636e91e07f6bc401c2651a
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-06-12 14:55:41 -0300

    feat: add Jaeguer to make tracing

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,3 +1,6 @@
+import "@opentelemetry/auto-instrumentations-node/register";
+import { trace } from "@opentelemetry/api";
+
 import { fastifyCors } from "@fastify/cors";
 import { z } from "zod";
 import {
@@ -10,6 +13,8 @@ import { channels } from "../broker/channels/index.ts";
 import { db } from "../db/client.ts";
 import { schema } from "../db/schema/index.ts";
 import { dispatchOrderCreated } from "../broker/messages/order-created.ts";
+import { setTimeout } from "node:timers/promises";
+import { tracer } from "../tracer/tracer.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -39,22 +44,25 @@ app.post(
 
     const orderId = crypto.randomUUID();
 
+    await db.insert(schema.orders).values({
+      id: orderId,
+      customerId: "dad73f3e-31c4-4f30-8e84-242094f62ddf",
+      amount,
+      status: "pending",
+    });
+
+    const span = tracer.startSpan("Eu acho que aqui tá dando erro ó");
+    span.setAttribute("teste", "Hello World!");
+    await setTimeout(2000);
+    span.end();
+
+    trace.getActiveSpan()?.setAttribute("order_id", orderId);
+
     dispatchOrderCreated({
       orderId,
       amount,
       customer: { id: "dad73f3e-31c4-4f30-8e84-242094f62ddf" },
     });
-
-    try {
-      await db.insert(schema.orders).values({
-        id: orderId,
-        customerId: "dad73f3e-31c4-4f30-8e84-242094f62ddf",
-        amount,
-        status: "pending",
-      });
-    } catch (err) {
-      console.log(err);
-    }
 
     return reply.status(201).send();
   }
